aaf46da40051c59f81b712df0a0a82d2ba755ad4
davidsonbr12 <davidsonbr12@gmail.com>
2023-02-15 12:40:45 -0800

    band name generator and tip calculator initial builds

---
+++ pythonProject/band_name_generator.py
@@ -0,0 +1,6 @@
+print("This is a band name generator")
+
+city = input("Please enter the city where you were born: ")
+pet = input("Please enter the name of your first pet: ")
+
+print(f"Your band name could be {city} {pet}.")
---
+++ pythonProject/tip_calculator.py
@@ -0,0 +1,29 @@
+# A simple tip calculator.  Prints the total plus tip per person.
+
+print("Welcome to the tip calculator.")
+while True:
+    bill = float(input("What was the total bill? $"))
+    if bill < 0:
+        print("Please enter a positive value for the total.")
+    else:
+        break
+while True:
+    tip_amount = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+    if tip_amount not in (10, 12, 15):
+        print("Please select from the provided tip options.")
+    else:
+        break
+while True:
+    party_size = int(input("How many people to split the bill? "))
+    if party_size < 1:
+        print("Party size must be 1 or more.")
+    else:
+        break
+
+tip_percent = tip_amount / 100
+total_tip_amount = bill * tip_percent
+total_bill_amount = bill + total_tip_amount
+total_per_person = round(total_bill_amount / party_size, 2)
+final_bill = "{:.2f}".format(total_per_person)
+
+print(f"Each person should pay: ${final_bill}")
